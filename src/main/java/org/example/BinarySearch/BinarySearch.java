2205ed22a13e1f58c5c0045ce5025b51fd344b56
xc <3136493582@qq.com>
2024-12-12 20:01:00 +0800

    完成平衡版本和java的二分查找以及测试通过

--- src/main/java/org/example/BinarySearch/BinarySearch.java
+++ src/main/java/org/example/BinarySearch/BinarySearch.java
@@ -47,4 +47,48 @@ public class BinarySearch {
         //没找到返回-1
         return -1;
     }
+
+    /**
+     * 二分查找平衡版 不奢望循环内通过 mid找出目标, 缩小区间直至剩 1 个, 剩下的这个可能就是要找的（通过 low）,左闭右开的区间，low 指向的可能是目标，而 high 指向的不是目标
+     * @param arr 传入的数组
+     * @param target 目标值
+     * @return 找到则返回目标值，没有找到则返回-1
+     */
+    public static int binarySearchBalance(int[] arr, int target) {
+        int low = 0;
+        int high = arr.length ;
+        while (1<high-low) {
+            int mid = (low + high) >>>1;
+            if(target<arr[mid]) {
+                high = mid ;
+            }
+            else {
+                low = mid;
+            }
+        }
+        return (arr[low] == target) ? low : -1;
+    }
+
+    /**
+     * 二分查找 java版
+     * @param arr 传入的数组
+     * @param fromIndex  起始指针
+     * @param toIndex    终止指针
+     * @param key        查找目标
+     * @return  例如 $[1,3,5,6]$ 要插入 $2$ 那么就是找到一个位置，这个位置左侧元素都比它小，等循环结束，若没找到，low 左侧元素肯定都比 target 小，因此 low 即插入点
+     */
+    public static int binarySearchJava(int[] arr, int fromIndex, int toIndex, long key) {
+        int low = fromIndex;
+        int high = toIndex - 1;
+        while (low <= high) {
+            int mid = (low + high) >>>1;
+            long midVal = arr[mid];
+            if(key<midVal) {
+                high = mid - 1;
+            }else if(key>midVal) {
+                low = mid + 1;
+            }else return mid;
+        }
+        return -(low+1);//-1 是为了把索引 0 位置的插入点与找到的情况进行区分
+    }
 }
--- src/test/java/TestBinarySearch.java
+++ src/test/java/TestBinarySearch.java
@@ -2,6 +2,9 @@ import org.example.BinarySearch.BinarySearch;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+/**
+ * 二分查找测试类
+ */
 public class TestBinarySearch {
     @Test
     void testBinarySearchBase() {
@@ -24,4 +27,22 @@ public class TestBinarySearch {
         Assertions.assertEquals(-1, BinarySearch.binarySearchPlus(arr,120));
         Assertions.assertEquals(50, BinarySearch.binarySearchPlus(arr,50));
     }
+
+    @Test
+    void testBinarySearchBalance(){
+        int[] arr=new int[]{-1,5,8,9,15,25,45,77};
+        Assertions.assertEquals(5, BinarySearch.binarySearchBalance(arr,25));
+        Assertions.assertEquals(0, BinarySearch.binarySearchBalance(arr,-1));
+        Assertions.assertEquals(6, BinarySearch.binarySearchBalance(arr,45));
+    }
+
+    @Test
+    void testBinarySearchJava(){
+        int[] arr=new int[]{-1,5,8,9,15,25,45,77};
+        Assertions.assertEquals(5, BinarySearch.binarySearchJava(arr,2,8,25));
+        Assertions.assertEquals(7, BinarySearch.binarySearchJava(arr,2,8,77));
+        Assertions.assertEquals(-7, BinarySearch.binarySearchJava(arr,2,8,30));
+        Assertions.assertEquals(1, BinarySearch.binarySearchJava(arr,0,8,5));
+    }
+
 }
